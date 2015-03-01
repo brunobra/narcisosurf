4c959798e168bb62e3ba2cb8c5fa7ffacb5c7d5c
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2015-03-01 20:43:25 -0300

    "Starting to build Images Gallery"

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,16 @@
+/********************
+		MENU
+*********************/
+
 $('.menu-anchor').on('click touchstart', function(e){
 	$('html').toggleClass('menu-active');
   	e.preventDefault();
+});
+
+/********************
+		GALERIA
+*********************/
+
+$.getJSON("./img/gallery/images.json", function(data){
+	console.log(data)
 });
