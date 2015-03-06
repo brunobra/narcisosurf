4d93e84fbff9c1746b117ae8b757b0cc8b199194
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2015-03-06 18:35:55 -0300

    "Fixing styling and layout for Home page, put maps on bottom"

--- static/js/app.js
+++ static/js/app.js
@@ -6,11 +6,3 @@ $('.menu-anchor').on('click touchstart', function(e){
 	$('html').toggleClass('menu-active');
   	e.preventDefault();
 });
-
-/********************
-		GALERIA
-*********************/
-
-$.getJSON("./static/img/gallery/images.json", function(data){
-	console.log(data)
-});
